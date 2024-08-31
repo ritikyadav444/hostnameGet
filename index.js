22056c184b11d89b624745618927e3910906de76
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2024-08-31 18:40:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const express = require('express');
 const geoip = require('geoip-lite');
-const app = express();
 const dns = require('dns');
-const PORT = process.env.PORT || 10000
+
+const app = express();
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
-
-    const clientIp = req.connection.remoteAddress;
-
-    dns.reverse(clientIp, (err, hostnames) => {
-        if (err) {
-            res.send('Hostname not found');
-        } else {
-            res.send(`Client Hostname: ${hostnames[0]}`);
-        }
-    });
-
+    // Retrieve IP address from headers or socket
     const ip =
         req.headers['cf-connecting-ip'] ||
         req.headers['x-real-ip'] ||
-        (req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : null) || req.socket.remoteAddress
+        (req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : null) || req.socket.remoteAddress;
 
-    console.log('IP Retrieved:', ip);
+    // Initialize response variables
+    let responseText = '';
+    let hostnameResolved = false;
+    let geoResolved = false;
+
+    // Perform DNS reverse lookup
+    dns.reverse(ip, (err, hostnames) => {
+        if (err) {
+            responseText += 'Hostname not found. ';
+        } else {
+            responseText += `Client Hostname: ${hostnames[0]}. `;
+        }
+        hostnameResolved = true;
+        if (geoResolved) {
+            res.send(responseText);
+        }
+    });
+
+    // Lookup geolocation
     const geo = geoip.lookup(ip);
-
     if (!geo) {
-        console.log('Geolocation not found.');
-        return res.send('Geolocation not found.');
+        responseText += 'Geolocation not found. ';
+    } else {
+        responseText += `IP: ${ip}, Your location: ${geo.city}, Country: ${geo.country}, Region: ${geo.region}, Timezone: ${geo.timezone}, LL: ${geo.ll}, Range: ${geo.range}. `;
     }
-
-    console.log('Geo Information:', geo);
-    res.send(` ip:${ip} Your location: ${geo.city}, Country:${geo.country} ,Region: ${geo.region} ,Timezone: ${geo.timezone},LL ${geo.ll} Range:${geo.range}`);
+    geoResolved = true;
+    if (hostnameResolved) {
+        res.send(responseText);
+    }
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is working on http://192.168.1.151:${PORT}`)
-
-})
+    console.log(`Server is running on http://192.168.1.151:${PORT}`);
+});
