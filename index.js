fbbccbc28072718b0511316e93fa13c235a9c766
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2024-08-31 18:02:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
-const dns = require('dns');
+const geoip = require('geoip-lite');
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 10000
 
 app.get('/', (req, res) => {
-    const ip = req.ip
-    res.render(ip)
+    const ip =
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-real-ip'] ||
+        (req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : null) || req.socket.remoteAddress
+
+    console.log('IP Retrieved:', ip);
+    const geo = geoip.lookup(ip);
+
+    if (!geo) {
+        console.log('Geolocation not found.');
+        return res.send('Geolocation not found.');
+    }
+
+    console.log('Geo Information:', geo);
+    res.send(`Your location: ${geo.city}, ${geo.country}`);
 });
 
+const server = app.listen(PORT, () => {
+    console.log(`Server is working on http://192.168.1.151:${PORT}`)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+})
