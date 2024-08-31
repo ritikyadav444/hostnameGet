f2387f1bbcff92475148bb3a6ac089f459af1826
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2024-08-31 17:45:31 +0530

    Update32 index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const requestIp = require('request-ip');
 const dns = require('dns');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 10000 ;
-
-app.use(requestIp.mw());
-app.set('view engine', 'ejs');
-app.get('/ho', (req, res) => {
-    res.render('test');
-});
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
     const clientIp = req.connection.remoteAddress;
@@ -17,23 +9,23 @@ app.get('/', (req, res) => {
 
     if (normalizedIp.startsWith('192.168.') || normalizedIp.startsWith('10.') || normalizedIp.startsWith('172.16.') || normalizedIp.startsWith('127.') || normalizedIp === '::1') {
         const message = `Private or localhost IP detected: ${normalizedIp}`;
-        res.render('index', { hostname: null, message });
+        res.json({ hostname: null, message });
         console.log(`Private or localhost IP: ${normalizedIp}`);
     } else {
         dns.reverse(normalizedIp, (err, hostnames) => {
             if (err) {
                 const message = `Could not resolve hostname for IP: ${normalizedIp}`;
-                res.render('index', { hostname: null, message });
+                res.json({ hostname: null, message });
                 console.log("DNS error:", err.message);
             } else {
                 const hostname = hostnames[0];
-                res.render('index', { hostname, message: null });
+                res.json({ hostname, message: null });
                 console.log("Resolved Hostnames:", hostnames);
             }
         });
     }
 });
 
-const server = app.listen(PORT, () => {
-    console.log(`Server is working on http://192.168.1.151:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
