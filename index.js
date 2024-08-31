54dd7447010655b681e3daccc48414a6049097a9
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2024-08-31 17:55:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
-    const clientIp = req.connection.remoteAddress;
-    const normalizedIp = clientIp.startsWith('::ffff:') ? clientIp.split('::ffff:')[1] : clientIp;
-
-    dns.reverse(normalizedIp, (err, hostnames) => {
-        if (err) {
-            const message = `Could not resolve hostname for IP: ${normalizedIp}`;
-            res.json({ hostname: null, message });
-            console.log("DNS error:", err.message);
-        } else {
-            const hostname = hostnames[0];
-            res.json({ hostname, message: null });
-            console.log("Resolved Hostnames:", hostnames);
-        }
-    });
+    const ip = req.ip
+    res.render(ip)
 });
 
 
